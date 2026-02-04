feb770c445e74e86e26abd83836c248addca20a8
FerdiStro <ferdistr08@gmail.com>
2026-02-05 00:21:42 +0100

    Feat: Start up all Finders. Needed to be Tested

--- BeatBoxer-Engien/src/main/java/com/FerdiStro/cdj/VirtualDevice.java
+++ BeatBoxer-Engien/src/main/java/com/FerdiStro/cdj/VirtualDevice.java
@@ -7,19 +7,25 @@ import org.apache.logging.log4j.Logger;
 import org.deepsymmetry.beatlink.BeatFinder;
 import org.deepsymmetry.beatlink.BeatListener;
 import org.deepsymmetry.beatlink.VirtualCdj;
+import org.deepsymmetry.beatlink.data.ArtFinder;
+import org.deepsymmetry.beatlink.data.BeatGridFinder;
+import org.deepsymmetry.beatlink.data.MetadataFinder;
+import org.deepsymmetry.beatlink.data.WaveformFinder;
+
+import java.io.IOException;
 
 public class VirtualDevice {
 
 
     protected static final Logger log = LogManager.getLogger();
-    private static final  Byte DEVICE_NUMBER = 4;
+    private static final Byte DEVICE_NUMBER = 4;
 
-    private static  VirtualDevice  INSTANCE = null;
+    private static VirtualDevice INSTANCE = null;
 
 
-    private VirtualDevice(){
+    private VirtualDevice() {
         log.info(LogUtils.LINE_SEPARATOR);
-        log.info("Starting Virtual DJ on Device Number  {}",DEVICE_NUMBER);
+        log.info("Starting Virtual DJ on Device Number  {}", DEVICE_NUMBER);
         VirtualCdj cdj = VirtualCdj.getInstance();
         cdj.setDeviceNumber(DEVICE_NUMBER);
 
@@ -34,20 +40,40 @@ public class VirtualDevice {
         cdj.setSynced(true);
         cdj.setPlaying(true);
 
+        log.info("Enable sending status to other CDJs");
+        try {
+            cdj.setSendingStatus(true);
+        } catch (IOException e) {
+            log.error(e.toString());
+            throw new RuntimeException(e);
+        }
+
+        log.info("Start all finders");
+        try {
+            MetadataFinder.getInstance().start();
+            ArtFinder.getInstance().start();
+            WaveformFinder.getInstance().start();
+            BeatGridFinder.getInstance().start();
+            BeatFinder.getInstance().start();
+        } catch (Exception e) {
+            log.error(e.toString());
+            throw new IllegalStateException();
+        }
+
         BeatFinder beatFinder = BeatFinder.getInstance();
 
-        if(!beatFinder.isRunning()){
+        if (!beatFinder.isRunning()) {
             throw new BeatFinderNotRunningException(cdj.toString());
         }
     }
 
-    public void addBeatListener(BeatListener beatListener){
+    public void addBeatListener(BeatListener beatListener) {
 
     }
 
 
     public static VirtualDevice getInstance() {
-        if(INSTANCE == null){
+        if (INSTANCE == null) {
             INSTANCE = new VirtualDevice();
         }
         return INSTANCE;
